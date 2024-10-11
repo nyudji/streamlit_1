3e35d14c4d82f88258b9666d23c67c83831bd0fb
Nicolas Yudji <144136051+nyudji@users.noreply.github.com>
2024-10-11 05:50:46 -0300

    main: Arrumando Home no Deplay

--- Home.py
+++ Home.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pandas as pd
+import pages.Pizza as pg_pizza  # Importe suas páginas
+import pages.Contratos as pg_contratos
 
 st.set_page_config(page_title='Home')
 
 st.title('Home')
 st.sidebar.success('Selecione uma página acima')
-st.write('Pagina principal Streamlit')
 
+
+page = 'Home'
+# Menu de navegação
+page = st.sidebar.selectbox("Navegação", ["Home", "Pizza", "Contratos"])
+
+# Função para carregar a página selecionada
+if page == "Home":
+    st.write("Bem-vindo à Página Principal!")
+else:
+    # Lê o conteúdo do arquivo da página e executa o código
+    with open(f"pages/{page}.py") as file:
+        exec(file.read())
 
     
