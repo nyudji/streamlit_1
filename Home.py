1aaa4da7453f9d1c2f1e5f819d968efd1a4e189b
Nicolas Yudji <144136051+nyudji@users.noreply.github.com>
2024-10-11 05:15:29 -0300

    Add files via upload
    main:Colocando todos os arquivos do estudo

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Streamlit')
+
+st.title('Home')
+st.sidebar.success('Selecione uma página acima')
+st.write('Pagina principal Streamlit')
+
+
+    
