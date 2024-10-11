59594f4a2daa314cb8a570f524d06f39907457ef
Nicolas Yudji <144136051+nyudji@users.noreply.github.com>
2024-10-11 05:58:40 -0300

    update teste

--- Home.py
+++ Home.py
@@ -8,12 +8,13 @@ st.title('Home')
 st.sidebar.success('Selecione uma página acima')
 
 
+# Função para carregar a página selecionada
 if page == "Home":
     st.write("Bem-vindo à Página Principal!")
 else:
-    # Construindo o caminho completo para o arquivo da página selecionada
-    base_dir = os.path.dirname(__file__)  # Caminho do arquivo atual (Home.py)
-    file_path = os.path.join(base_dir, "pages", f"{page}.py")  # Caminho para o arquivo da página
+    # Construindo o caminho para o arquivo da página selecionada
+    base_dir = os.path.dirname(__file__)  # Diretório do arquivo atual (Home.py)
+    file_path = os.path.join(base_dir, f"{page}.py")  # Caminho para o arquivo da página
 
     # Verifica se o arquivo existe antes de tentar carregá-lo
     if os.path.exists(file_path):
@@ -21,5 +22,4 @@ else:
             exec(file.read())
     else:
         st.error("Página não encontrada.")
-
     
